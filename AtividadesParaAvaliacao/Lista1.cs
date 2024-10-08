a0e99c14ea3005c712d593023c32360022c9a956
JULIO CESAR GOMES <julio.cgomes8@senacsp.edu.br>
2024-10-07 22:33:12 -0300

    Primeira versão das atividades

---
+++ AtividadesParaAvaliacao/Lista1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadesParaAvaliacao
+{
+    internal class Lista1
+    {
+        public  void exercicio1()
+        {
+            Console.Clear();
+            
+            string nome;
+            Console.WriteLine(" digite seu nome ");
+            nome = Console.ReadLine();
+            Console.WriteLine(" Ola " + nome + " Prazer em te conhecer \n ");
+
+            Console.WriteLine("digite seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine(" tecnico" + nome + " Salario 3.000,00 \n");
+
+
+
+        }
+    }
+}
---
+++ AtividadesParaAvaliacao/Lista2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadesParaAvaliacao
+{
+    internal class Lista2
+    {
+        public void exercicio2() 
+        {
+            Console.WriteLine("lista 2"  );
+        } 
+    }
+}
